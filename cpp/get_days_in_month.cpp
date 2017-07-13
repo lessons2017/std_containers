b7547c7f917112b7bff0dbfb500767b522edb950
seeplusplus <seeplusplus@yandex.ru>
2017-07-13 22:36:40 +0300

    initial c++ commit

---
+++ cpp/get_days_in_month.cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+#include <vector>
+using namespace std; 
+
+void days_in_a_month(const vector<size_t> & year)
+{
+ int month; 
+ cin >> month; 
+ cout << year.at(month-1) << endl;
+ }
+ 
+
+int main() 
+{
+
+ vector<size_t> year {31, 28, 31, 30, 31, 30,
+  31, 31, 30, 31, 30, 31};
+ cout << "insert month number" << endl; 
+ try
+ {
+  for (int i = 0; i != 3; ++i)
+  {
+   days_in_a_month(year);
+  }
+ }
+
+ catch (...) {
+  cout << "task failed" << endl;
+ }
+ system ("pause"); 
+
+ return 0;
+}
